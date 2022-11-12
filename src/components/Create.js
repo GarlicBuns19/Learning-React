5fea936d7867ebd40facdfe1c915ec7149ddc385
GarlicBuns19 <planterx26@gmail.com>
2022-11-12 17:14:18 +0100

    Adding data

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,16 +2,37 @@ import { useState } from "react";
 
 function Create() {
     const [title,setTitle] = useState('')
+    const [pending,setPending] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setPending(true)
+        let blog = {title}
+        console.log(blog)
+        setTitle('')
+
+        fetch('http://localhost:6969/results',{
+            method: 'POST',
+            headers : {'Content-Type': 'application/json'},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            setPending(false)
+            console.log('new blog added')
+        })
+    }
+
     return ( 
         <div className="create">
             <h1>Add a blog plz</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Title</label>
                 <input 
                 type="text"
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
                 />
+                {!pending && <button>Post</button>}
+                {pending && <button disabled>Adding....</button>}
                 <p>{title}</p>
             </form>
         </div>
